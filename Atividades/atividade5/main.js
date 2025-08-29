bd250d9707aef21a51e77649ec45cb66f607caf8
muriloliro0 <murilo.daniel.cunha@escola.pr.gov.br>
2025-08-29 09:27:05 -0300

    Add files via upload

---
+++ Atividades/atividade5/main.js
@@ -0,0 +1,12 @@
+const frm = document.querySelector("form")
+const resp1 = document.querySelector("#outMedicamento")
+const resp2 = document.querySelector("#outPromocao")
+
+frm.addEventListener("submit", (e) => {
+    const medicamento = frm.inMedicamento.value
+    const preco = Number(frm.inPreco.value)
+    const promocao = Math.floor(preco * 2)
+    resp1.innerText = `Promoção de ${medicamento}`
+    resp2.innerText = `Leve 2 por apenas R$ ${promocao.toFixed(2)}`
+    e.preventDefault()
+})
